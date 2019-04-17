7d9e29ee61089bfaefc7458df01fe9f17de2820d
Tim <timjlucassen@gmail.com>
2019-04-17 17:12:38 +0200

    Updated the animation node to debug bone locations and Evaluate base pose before applying rotations

--- Source/SurvivalGame/Animation/AnimNode_PFNN.cpp
+++ Source/SurvivalGame/Animation/AnimNode_PFNN.cpp
@@ -76,8 +76,6 @@ void FAnimNode_PFNN::ApplyPFNN(FPoseContext& arg_LocalPoseContext)
 
 	FCSPose<FCompactPose> GlobalPose;
 	GlobalPose.InitPose(arg_LocalPoseContext.Pose);
-
-
 }
 
 void FAnimNode_PFNN::PredictFutureTrajectory(UTrajectoryComponent* arg_Trajectory)
@@ -114,22 +112,16 @@ void FAnimNode_PFNN::Update_AnyThread(const FAnimationUpdateContext& Context)
 
 void FAnimNode_PFNN::Evaluate_AnyThread(FPoseContext& Output)
 {
-
-
-	for(int32 i = 0; i < JOINT_NUM; i++)
-	{
-		const FCompactPoseBoneIndex index(i);
-
-		UE_LOG(LogTemp, Error, TEXT("Evaluate"));
-
-		const FCompactPoseBoneIndex RootBoneIndex(i);
-		Output.Pose[RootBoneIndex].SetRotation(Output.Pose[RootBoneIndex].GetRotation());
-		Output.Pose[RootBoneIndex].NormalizeRotation();
-	}
-
-
-
-
-
 	BasePose.Evaluate(Output);
+
+	const FTransform& CharacterTransform = Output.AnimInstanceProxy->GetActorTransform();
+
+	for (int32 i = 0; i < Output.Pose.GetNumBones(); i++)
+	{
+		const FCompactPoseBoneIndex RootBoneIndex(i);
+		Output.Pose[RootBoneIndex].SetRotation(Output.Pose[RootBoneIndex].GetRotation() + FQuat(FQuat::MakeFromEuler(FVector(i * 3.0f, 0.0f, 0.0f))));
+
+		Output.AnimInstanceProxy->AnimDrawDebugSphere(Output.Pose[RootBoneIndex].GetLocation() + CharacterTransform.GetLocation(), 2.5f, 12, FColor::Green, false, -1.0f);
+	}
+	Output.Pose.NormalizeRotations();
 }
--- Source/SurvivalGame/Animation/PFNNAnimInstance.cpp
+++ Source/SurvivalGame/Animation/PFNNAnimInstance.cpp
@@ -2,23 +2,15 @@
 
 #include "PFNNAnimInstance.h"
 
+#include "DrawDebugHelpers.h"
+
 void UPFNNAnimInstance::NativeInitializeAnimation() 
 {
 	Super::NativeInitializeAnimation();
 
-	SkeletalMesh = GetOwningComponent();
 }
 
 void UPFNNAnimInstance::NativeUpdateAnimation(float arg_DeltaTimeX)
 {
 	Super::NativeUpdateAnimation(arg_DeltaTimeX);
-
-	if (!SkeletalMesh) 
-	{
-		UE_LOG(LogTemp, Error, TEXT("PFNNAnimationInstance does not have SkeletalMesh!"));
-		return;
-	}
-
-	const FBoneContainer& Bones = GetRequiredBones();
-
 }
