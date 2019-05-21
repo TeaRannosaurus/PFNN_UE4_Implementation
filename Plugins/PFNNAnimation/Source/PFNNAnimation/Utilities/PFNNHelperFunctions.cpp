abbbd26bc4ce46b480cb7d8a1fbbe0817d94f22d
Tim <timjlucassen@gmail.com>
2019-05-21 10:50:15 +0200

    Add Functions for flipping Translations

--- Plugins/PFNNAnimation/Source/PFNNAnimation/Utilities/PFNNHelperFunctions.cpp
+++ Plugins/PFNNAnimation/Source/PFNNAnimation/Utilities/PFNNHelperFunctions.cpp
@@ -3,3 +3,22 @@
 
 #include "PFNNHelperFunctions.h"
 
+FVector UPFNNHelperFunctions::XYZTranslationToXZY(const FVector& arg_TranslationVector)
+{
+	return FVector(-arg_TranslationVector.X, arg_TranslationVector.Z, arg_TranslationVector.Y);
+}
+
+FVector UPFNNHelperFunctions::XYZTranslationToXZY(const glm::vec3& arg_TranslationVector)
+{
+	return FVector(-arg_TranslationVector.x, arg_TranslationVector.z, arg_TranslationVector.y);
+}
+
+glm::vec3 UPFNNHelperFunctions::XZYTranslationToXYZ(const FVector& arg_TranslationVector)
+{
+	return glm::vec3(-arg_TranslationVector.X, arg_TranslationVector.Z, arg_TranslationVector.Y);
+}
+
+glm::vec3 UPFNNHelperFunctions::XZYTranslationToXYZ(const glm::vec3& arg_TranslationVector)
+{
+	return glm::vec3(-arg_TranslationVector.x, arg_TranslationVector.y, arg_TranslationVector.z);
+}
--- Plugins/PFNNAnimation/Source/PFNNAnimation/Utilities/PFNNHelperFunctions.h
+++ Plugins/PFNNAnimation/Source/PFNNAnimation/Utilities/PFNNHelperFunctions.h
@@ -2,8 +2,13 @@
 
 #pragma once
 
+
+
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+
+#include <ThirdParty/glm/glm.hpp>
+
 #include "PFNNHelperFunctions.generated.h"
 
 /**
@@ -14,4 +19,13 @@ class PFNNANIMATION_API UPFNNHelperFunctions : public UObject
 {
 	GENERATED_BODY()
 	
+public:
+
+	static FVector XYZTranslationToXZY(const FVector& arg_TranslationVector);
+	static FVector XYZTranslationToXZY(const glm::vec3& arg_TranslationVector);
+
+	static glm::vec3 XZYTranslationToXYZ(const FVector& arg_TranslationVector);
+	static glm::vec3 XZYTranslationToXYZ(const glm::vec3& arg_TranslationVector);
+
+
 };
